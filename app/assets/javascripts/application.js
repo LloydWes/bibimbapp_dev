e6094a386c631bb31f582ee4f6367b73fff7fda9
Lloyd <lloydwestbury@gmail.com>
2019-06-03 15:10:32 +0200

    Initial commit with template

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,52 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+//= require jquery/jquery.min
+//= require popper/popper.min
+//= require bootstrap/js/bootstrap.min
+//= require slidebar/slidebar
+//= require classie
+//= require bootstrap-notify/bootstrap-growl.min
+//= require scrollpos-styler/scrollpos-styler
+//= require adaptive-backgrounds/adaptive-backgrounds
+//= require countdown/js/jquery.countdown.min
+//= require dropzone/dropzone.min
+//= require easy-pie-chart/jquery.easypiechart.min
+//= require fancybox/js/jquery.fancybox.min
+//= require flatpickr/flatpickr.min
+//= require flip/flip.min
+//= require footer-reveal/footer-reveal.min
+//= require gradientify/jquery.gradientify.min
+//= require headroom/headroom.min
+//= require headroom/jquery.headroom.min
+//= require input-mask/input-mask.min
+//= require instafeed/instafeed
+//= require milestone-counter/jquery.countTo
+//= require nouislider/js/nouislider.min
+//= require paraxify/paraxify.min
+//= require select2/js/select2.min
+//= require sticky-kit/sticky-kit.min
+//= require swiper/js/swiper.min
+//= require textarea-autosize/autosize.min
+//= require typeahead/typeahead.bundle.min
+//= require typed/typed.min
+//= require vide/vide.min
+//= require viewport-checker/viewportchecker.min
+//= require wow/wow.min
+//= require isotope/isotope.min
+//= require imagesloaded/imagesloaded.pkgd.min
+//= require boomerang.min
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def pages
+  end
+end
