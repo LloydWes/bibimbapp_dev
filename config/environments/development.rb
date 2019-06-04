24749288c997fe517cec94c448a4aaa3dfb562f3
Lloyd <lloydwestbury@gmail.com>
2019-06-04 14:15:28 +0200

    Changed assets path in development back to default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.prefix = '/dev-assets'
+  # config.assets.prefix = '/dev-assets'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
