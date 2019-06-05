9bc8a9551c8963ff8f4109d66b0411b0a4d38ce8
notmoebius <notmoebius@gmail.com>
2019-06-05 11:44:04 +0200

    send welcome mail with sendgrid OK

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configuration for Sendgrid
+ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_LOGIN'],
+    :password => ENV['SENDGRID_PWD'],
+    :domain => 'bibimbapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
