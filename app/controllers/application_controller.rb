23c0306674a88d5266537f99305a8364f51cfcc4
notmoebius <notmoebius@gmail.com>
2019-06-05 16:58:38 +0200

    add method for devise in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :date_of_birth, :gender, :avatar, :id_admin?, :level_id)}
+    # devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :password_confirmation, :avatar, :birthday)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :date_of_birth, :gender, :avatar, :id_admin?, :level_id)}
+  end
 end
