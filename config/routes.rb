49878f8e0cb0860441848bf71916c84d6cec31bc
notmoebius <notmoebius@gmail.com>
2019-06-06 12:21:04 +0200

    install forum with route /forum done, need config

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   
   root to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # route for engine forum
+  mount Thredded::Engine => '/forum'
 end
