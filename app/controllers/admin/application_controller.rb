60979bc2d0138f9b67fa68fb97af9a13be8bf194
notmoebius <notmoebius@gmail.com>
2019-06-10 17:01:16 +0200

    admin ok for levels

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,14 +6,10 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    # before_action :authenticate_admin
+    before_action :authenticate_admin
     before_action :authenticate_user!
 
 
-    before_action do
-      flash[:danger] = "Access forbiden"
-      redirect_to root_path unless current_user.is_admin?
-    end
 
     def authenticate_admin
       current_user.is_admin?
