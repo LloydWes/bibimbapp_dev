012c999a2b71f41a72ac6ff11bcdd8028138946c
notmoebius <notmoebius@gmail.com>
2019-06-10 11:18:06 +0200

    update application controller for default routes sign_in, sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,16 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :password_confirmation, :avatar, :birthday)}
     devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :date_of_birth, :gender, :avatar, :id_admin?, :level_id)}
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    profile_path
+  end
+
+  def after_sign_out_path_for(resource)
+    # return the path based on resource
+    edit_user_registration
+  end
 end
