92e6f87246367f9c9a80884d79ca38f5fda90f6c
notmoebius <notmoebius@gmail.com>
2019-06-10 14:45:11 +0200

    logout ok with redirect to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     # return the path based on resource
-    profile_path
+    courses_path
   end
 
   def after_sign_out_path_for(resource)
     # return the path based on resource
-    edit_user_registration
+    root_path
   end
 end
