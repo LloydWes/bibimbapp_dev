b35de73bc93f0d91659277cf237e1a65fccc9a0c
notmoebius <notmoebius@gmail.com>
2019-06-11 17:15:03 +0200

    fix assessment_ansers syntax error

--- app/controllers/assessment_answers_controller.rb
+++ app/controllers/assessment_answers_controller.rb
@@ -7,14 +7,16 @@ class AssessmentAnswersController < ApplicationController
     respond_to do |format|
       if @assessment_answer.save
         delete_written
-        format.html { redirect_to assessment_url(@assessment), notice: ‘Assessment answers were successfully created.’ }
+        format.html { redirect_to assessment_url(@assessment), notice: "Assessment answers were successfully created." }
         format.json { render :show, status: :created, location: @assessment_answer }
       else
         format.html { render :new }
         format.json { render json: @assessment_answer.errors, status: :unprocessable_entity }
       end
     end
-    enddef assessment_answer_params(answers)
+  end
+  
+  def assessment_answer_params(answers)
     answers.permit(:letter, :answer, :correct, :assessment_question_id, :assessment_id)
   end
 
