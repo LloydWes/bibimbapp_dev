f153792cf67d316b48c2f486886474511922cccc
Lloyd <lloydwestbury@gmail.com>
2019-06-12 22:22:46 +0200

    Added date_of_birth validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   validates :gender, presence: true, allow_nil: false, inclusion: {in: %w(female male), message: "%{attribute} must be female or male"}
   validates :is_admin?, inclusion: {in: [false, true], message: "%{attribute} must be true or false"}
   # validates :level_id, presence: true, allow_nil: false#,inclusion: {in: Level.all.ids, message: "%{attribute} must be A1 A2 B1 B2 or #{Level.all.ids}"}
+  validates_date :date_of_birth, on_or_before: lambda { Date.current }, :message => "%{attribute} must be before today"
+
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
