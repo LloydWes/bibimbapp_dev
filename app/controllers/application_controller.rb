9eef9c509529df5de96f98c23a555d325dda52ac
notmoebius <notmoebius@gmail.com>
2019-06-14 15:59:27 +0200

    clean application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
 
   def configure_devise_parameters
-    if params['controller'] == 'devise/registrations'
-      if params['action'] == 'create' || params['action'] == 'update'
-        params[:user][:date_of_birth] = params[:user][:date_of_birth].to_date
-      end
-    end
+    # if params['controller'] == 'devise/registrations'
+    #   if params['action'] == 'create' || params['action'] == 'update'
+    #     params[:user][:date_of_birth] = params[:user][:date_of_birth].to_date
+    #   end
+    # end
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :date_of_birth, :gender)}
-    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :date_of_birth, :gender, :avatar, :is_admin?, :admin, :level_id)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :gender, :avatar, :is_admin?, :admin, :level_id)}
 
   end
 
