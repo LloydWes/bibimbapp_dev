a65fa716fff7a683df36e3cfe1428e5d3b413887
Py <pyrousseau@free.fr>
2019-06-14 13:56:11 +0200

    modif des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,35 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-    resources :lessons
-    resources :levels
-    resources :vocabularies
-    resources :assessments
-    resources :assessment_questions
-    resources :assessment_options
-    resources :assessment_answers
-    root to: "users#index"
-  end
 
-  devise_for :users
-  resources :users,  path: "/users", only: [:show]
-  authenticated :user do
-    root to: 'lessons#index', as: :authenticated_root
-  end
-  root to: 'landing#index'
+ root to: 'landing#index'
+ resources :lessons, only: [:index, :show]
+ resources :vocabularies, only: [:index]
 
-  resources :lessons
-  resources :vocabularies, only: [:index]
+ resources :lessons do
+   resources :assessments, only: [:create, :index] do
+     get 'take', to: 'assessments#new', on: :member, as: :new
+     get 'attempt/:attempt', to: 'assessments#show', on: :member, as: :show
+   end
+ end
 
-  # route for engine forum
-  mount Thredded::Engine => '/forum'
+ devise_for :users
+ resources :users,  path: "/users", only: [:show]
+ authenticated :user do
+   root to: 'lessons#index', as: :authenticated_root
+ end
 
-  resources :lessons do
-    resources :assessments, only: [:create, :index] do
-      get 'take', to: 'assessments#new', on: :member, as: :new
-      get 'attempt/:attempt', to: 'assessments#show', on: :member, as: :show
-    end
-  end
+ # route for admin
+ namespace :admin do
+   resources :users
+   resources :lessons
+   resources :levels
+   resources :vocabularies
+   resources :assessments
+   resources :assessment_questions
+   resources :assessment_options
+   resources :assessment_answers
+   root to: "users#index"
+ end
+
+ # route for engine forum
+ mount Thredded::Engine => '/forum'
 end
